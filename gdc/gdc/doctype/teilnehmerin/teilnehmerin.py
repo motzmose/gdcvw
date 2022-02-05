9a2e2229525e9621f400386e7f6a1a80e1e28dda
Moritz Kaimann <kaimann@edubuddy.net>
2022-02-05 14:55:00 +0100

    Automate Wirtschaftsregion

--- gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
+++ gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2022, didaktik-aktuell e.V. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Teilnehmerin(Document):
 	def before_save(self):
 		self.username = f'{self.vorname.lower()}_{self.nachname.lower()}'
+		plz = self.plz
+		doc = frappe.get_doc('PLZWR', plz)
+		self.wirtschaftsregion = doc.wirtschaftsregion
+
