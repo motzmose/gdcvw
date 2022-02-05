ffa229fc0c19c464c111823365ac2eecdd88f7fa
Moritz Kaimann <kaimann@edubuddy.net>
2022-02-05 17:13:36 +0100

    Autonaming and assurance of uniqueness

--- gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
+++ gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
@@ -6,8 +6,16 @@ from frappe.model.document import Document
 
 class Teilnehmerin(Document):
 	def before_save(self):
-		self.username = f'{self.vorname.lower()}_{self.nachname.lower()}'
 		plz = self.plz
 		doc = frappe.get_doc('PLZWR', plz)
 		self.wirtschaftsregion = doc.wirtschaftsregion
+	def before_naming(self):
+		num_retries = 100
+		nickname = f'{self.vorname.lower()}_{self.nachname.lower()}'
+		for attempt_no in range(num_retries):
+			if frappe.db.exists('Teilnehmerin', nickname):
+				nickname=f'{self.vorname.lower()}_{self.nachname.lower()}'+str(attempt_no)
+			else:
+				self.username = nickname
+				break
 
