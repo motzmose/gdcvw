81a85b0fa3363b8c8a3ef9515ef2d1934afb416f
Moritz Kaimann <kaimann@edubuddy.net>
2022-02-06 14:54:14 +0100

    Mail Autocreation

--- gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
+++ gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
@@ -2,20 +2,33 @@
 # For license information, please see license.txt
 
 import frappe
+#from urllib3 import Request, urlopen
+import requests
 from frappe.model.document import Document
 
-class Teilnehmerin(Document):
-	def before_save(self):
-		plz = self.plz
-		doc = frappe.get_doc('PLZWR', plz)
-		self.wirtschaftsregion = doc.wirtschaftsregion
-	def before_naming(self):
-		num_retries = 100
-		nickname = f'{self.vorname.lower()}_{self.nachname.lower()}'
-		for attempt_no in range(num_retries):
-			if frappe.db.exists('Teilnehmerin', nickname):
-				nickname=f'{self.vorname.lower()}_{self.nachname.lower()}'+str(attempt_no)
-			else:
-				self.username = nickname
-				break
 
+class Teilnehmerin(Document):
+    def before_save(self):
+        plz = self.plz
+        doc = frappe.get_doc('PLZWR', plz)
+        self.wirtschaftsregion = doc.wirtschaftsregion
+
+    def before_naming(self):
+        num_retries = 100
+        nickname = f'{self.vorname.lower()}_{self.nachname.lower()}'
+        for attempt_no in range(num_retries):
+            if frappe.db.exists('Teilnehmerin', nickname):
+                nickname = f'{self.vorname.lower()}_{self.nachname.lower()}' + \
+                    str(attempt_no)
+            else:
+                self.username = nickname
+                break
+
+    def after_insert(self):
+        settings = frappe.get_doc('GDC Settings')
+        values = {"local_part": self.username, "domain": "gdc-bw.de", "name": f"{self.vorname} {self.nachname}", "quota": "100",
+                  "password": settings.mcstdpw, "password2": settings.mcstdpw, "active": "1", "force_pw_update": "1", "tls_enforce_in": "0", "tls_enforce_out": "0"}
+        headers = {'Content-Type': 'application/json',
+                   'X-API-Key': settings.mcapi}
+        request = requests.post(
+            'https://mail.gdc-bw.de/api/v1/add/mailbox', json=values, headers=headers)
