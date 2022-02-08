2f3c3d52b4bdb484b1541b8ef65892048d1f0769
Moritz Kaimann <kaimann@edubuddy.net>
2022-02-08 18:07:20 +0100

    API Call to Moodle

--- gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
+++ gdc/gdc/doctype/teilnehmerin/teilnehmerin.py
@@ -5,15 +5,18 @@ import frappe
 import requests
 import json
 from frappe.model.document import Document
-
-
+#from moodle import Moodle
 
 class Teilnehmerin(Document):
+
+    # Read Wirtschaftsregion from Document indexed by zipcode
     def before_save(self):
         plz = self.plz
         doc = frappe.get_doc('PLZWR', plz)
         self.wirtschaftsregion = doc.wirtschaftsregion
+        self.email = f'{self.username}@gdc-bw.de'
 
+    # Create Username based on Firstname and Lastname before the Document gets its name
     def before_naming(self):
         num_retries = 100
         nickname = f'{self.vorname.lower()}_{self.nachname.lower()}'
@@ -25,15 +28,21 @@ class Teilnehmerin(Document):
                 self.username = nickname
                 break
 
+    # Controller Method to start API requests after the Document has been created
     def after_insert(self):
+        # Fetch Settings from Single DocType 'GDC Settings'
         settings = frappe.get_doc('GDC Settings')
-        values = {"local_part": self.username, "domain": "gdc-bw.de", "name": f"{self.vorname} {self.nachname}", "quota": "100",
-                  "password": settings.mcstdpw, "password2": settings.mcstdpw, "active": "1", "force_pw_update": "1", "tls_enforce_in": "0", "tls_enforce_out": "0"}
-        headers = {'Content-Type': 'application/json',
-                   'X-API-Key': settings.mcapi}
-        request = requests.post(
-            'https://mail.gdc-bw.de/api/v1/add/mailbox', json=values, headers=headers)
-
+        # API Request to Mailcow creating a new Mailbox
+        mc_values = {"local_part": self.username, "domain": "gdc-bw.de", "name": f"{self.vorname} {self.nachname}", "quota": "100",
+                     "password": settings.mcstdpw, "password2": settings.mcstdpw, "active": "1", "force_pw_update": "1", "tls_enforce_in": "0", "tls_enforce_out": "0"}
+        mc_headers = {'Content-Type': 'application/json',
+                      'X-API-Key': settings.mcapi}
+        mc_request = requests.post(
+            f'https://{settings.mcdomain}/api/v1/add/mailbox', json=mc_values, headers=mc_headers)
+        # API Request to Moodle creating a new Account
+        mdl_params = {"wstoken": settings.mdl_api_key, "moodlewsrestformat": 'json', "wsfunction": 'core_user_create_users', "password":settings.mdl_std_pwd, "username": self.username, "firstname": self.vorname, "lastname": self.nachname, "email": self.email, "city": self.stadt}
+        mdl_request = requests.post(f'https://{settings.mdl_domain}/webservice/rest/server.php', mdl_params)
+        print(mdl_request)
 @frappe.whitelist()
 def resetmail(doc: str):
     doc_dict = json.loads(doc)
@@ -48,4 +57,4 @@ def resetmail(doc: str):
             }
         }
     headers = {'Content-Type': 'application/json','X-API-Key': settings.mcapi}
-    request = requests.post('https://mail.gdc-bw.de/api/v1/edit/mailbox', json=values, headers=headers)
+    request = requests.post(f'https://{settings.mdl_domain}/api/v1/edit/mailbox', json=values, headers=headers)
