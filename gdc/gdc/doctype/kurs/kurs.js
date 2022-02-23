e9a2b8ba6694c0ae24eabcb9ba66478fef40e9f9
Moritz Kaimann <moritz@kaimann.net>
2022-02-23 13:37:04 +0100

    Implement Course Creation

--- gdc/gdc/doctype/kurs/kurs.js
+++ gdc/gdc/doctype/kurs/kurs.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Kurs', {
-	// refresh: function(frm) {
-
-	// }
+    gruppe: function (frm) {
+        if (frm.doc.gruppe) {
+            frm.clear_table('teilnehmerinnen');
+            frappe.model.with_doc('Gruppe', frm.doc.gruppe, function () {
+                let source_doc = frappe.model.get_doc('Gruppe', frm.doc.gruppe);
+                $.each(source_doc.teilnehmerinnen, function (index, source_row) {
+                    frm.add_child('teilnehmerinnen').teilnehmerin = source_row.teilnehmerin;
+					frm.add_child('teilnehmerinnen').tn_name = source_row.tn_name; // this table has only one column. You might want to fill more columns.
+                    frm.refresh_field('teilnehmerinnen');
+                });
+            });
+        }
+    },
 });
--- gdc/gdc/doctype/teilnehmerin_kurs/teilnehmerin_kurs.py
+++ gdc/gdc/doctype/teilnehmerin_kurs/teilnehmerin_kurs.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2022, didaktik-aktuell e.V. and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
-class Kurs(Document):
+class TeilnehmerinKurs(Document):
 	pass
