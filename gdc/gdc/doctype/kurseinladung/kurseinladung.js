8959fd5df3e0974d25f094097ef6526bf7592c72
Moritz Kaimann <moritz@kaimann.net>
2022-03-25 10:05:11 +0100

    Invitation automation

--- gdc/gdc/doctype/kurseinladung/kurseinladung.js
+++ gdc/gdc/doctype/kurseinladung/kurseinladung.js
@@ -6,3 +6,28 @@ frappe.ui.form.on('Kurseinladung', {
 
 	// }
 });
+
+frappe.ui.form.on("Kurseinladung", "loadtn", function(frm) {
+	if(frm.doc.kurse){
+		const future_tn = frappe.call({
+			method: "gdc.gdc.doctype.kurseinladung.kurseinladung.loadtn",
+			freeze: true,
+			args: {
+				args: {
+					"kurs": frm.doc.kurse
+				}
+			}
+		}).then(r => {
+			for (let tn in r.message){
+				console.log(r.message[tn])
+				console.log(tn)
+				let row = frm.add_child('teilnehmerinnen',{teilnehmerin: r.message[tn]});
+			}
+			frm.refresh_field('teilnehmerinnen');
+		});
+		
+		//console.log(future_tn)
+		//frm.refresh_field('teilnehmerinnen');
+		//console.log(frm.doc.teilnehmerinnen)
+	};
+});
--- gdc/gdc/doctype/kurseinladung/kurseinladung.py
+++ gdc/gdc/doctype/kurseinladung/kurseinladung.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2022, didaktik-aktuell e.V. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import json
 from frappe.model.document import Document
 
 class Kurseinladung(Document):
 	pass
+
+@frappe.whitelist()
+def loadtn(args):
+	future_tn = []
+	args_dict = json.loads(args)
+	kuerzel = frappe.get_doc("Kurs",args_dict["kurs"][0]["kurs"]).kuerzel
+	teilnehmerinnen = frappe.db.get_list("Teilnehmerin", pluck="name")
+	for teilnehmerin in teilnehmerinnen:
+		tn_doc = frappe.get_doc("Teilnehmerin", teilnehmerin)
+		future_tn.append(tn_doc.name)
+		for tag in tn_doc.get_tags():
+			if tag == kuerzel:
+				future_tn.pop(-1)
+				print(f"Popped {tn_doc.name}")
+	print(future_tn)
+	return future_tn<
