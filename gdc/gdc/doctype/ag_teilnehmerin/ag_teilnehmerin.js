e7b4d6786b342f2c3c260ee15d77a7ff698dc023
Moritz Kaimann <moritz@kaimann.net>
2022-06-20 12:41:12 +0200

    Process changed -> new model

---
+++ gdc/gdc/doctype/ag_teilnehmerin/ag_teilnehmerin.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, didaktik-aktuell e.V. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('AG Teilnehmerin', {
+	// refresh: function(frm) {
+
+	// }
+});
