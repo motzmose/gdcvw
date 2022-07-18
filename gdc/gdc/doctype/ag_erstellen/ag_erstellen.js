8387d4931f40af08f14d0a7bdc8e8118bf3906da
Moritz Kaimann <moritz@kaimann.net>
2022-07-18 23:26:08 +0200

    Further changes to tools

--- gdc/gdc/doctype/ag_erstellen/ag_erstellen.js
+++ gdc/gdc/doctype/ag_erstellen/ag_erstellen.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('AG erstellen', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.disable_save();
+		frm.set_value('dauer', 90);
+	}
+});
 
-	// }
+frappe.ui.form.on("AG erstellen", "create", function(frm) {
+	if(frm.doc.erster_termin && frm.doc.letzter_termin){
+		frappe.call({
+			method: "gdc.gdc.doctype.ag_erstellen.ag_erstellen.insert",
+			freeze: true,
+			args: {
+				args: {
+					"erster_termin": frm.doc.erster_termin,
+					"letzter_termin": frm.doc.letzter_termin,
+					"dauer": frm.doc.dauer,
+					"wochenende": frm.doc.wochenende,
+					"wiederholung": frm.doc.wiederholung,
+					"tutorin": frm.doc.tutorin
+				}
+			}
+		})
+		frappe.msgprint(__('Kurs wurde erstellt.'));
+	}
+	else {
+		frappe.msgprint(__('Kurs konnte nicht erstellt werden. Es ist zumindest ein Beginn und Ende erforderlich.'));
+	}
 });
--- gdc/gdc/doctype/ag_erstellen/ag_erstellen.py
+++ gdc/gdc/doctype/ag_erstellen/ag_erstellen.py
@@ -1,8 +1,36 @@
 # Copyright (c) 2022, didaktik-aktuell e.V. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import json
+import datetime
 from frappe.model.document import Document
 
 class AGerstellen(Document):
 	pass
+
+@frappe.whitelist()
+def insert(args):
+	args = json.loads(args)
+	begin = args["erster_termin"]
+	end = args["letzter_termin"]
+	dauer = args["dauer"]
+	repeat = args["wiederholung"]
+	ag = frappe.new_doc("AG")
+	date = begin
+	while frappe.utils.getdate(end) > frappe.utils.getdate(date):
+		ag.append("termine",{
+			"termin" : date,
+			"ende" : frappe.utils.add_to_date(date, minutes=dauer)
+		})
+		if repeat=="Wöchentlich":
+			date = frappe.utils.add_to_date(date, weeks=1)
+		elif repeat=="Täglich":
+			date = frappe.utils.add_to_date(date, days=1)
+			if frappe.utils.getdate(date).weekday() > 4 and not args["wochenende"]:
+				date = frappe.utils.add_to_date(date, days=2)
+			else:
+				pass
+	try: ag.tutor = args["tutorin"]
+	except: ag.tutor = ""
+	ag.insert()
