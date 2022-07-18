2987afc14bbf28129165973dc6fa80c22546303e
Moritz Kaimann <moritz@kaimann.net>
2022-07-18 14:11:50 +0200

    New Tool for creating ec

--- gdc/gdc/doctype/kurs/kurs.py
+++ gdc/gdc/doctype/kurs/kurs.py
@@ -5,8 +5,4 @@ import frappe
 from frappe.model.document import Document
 
 class Kurs(Document):
-	def after_insert(self):
-		for kurstermin in self.kurstermine:
-			kurstermin.ende = frappe.utils.add_to_date(kurstermin.termin, hours=2)
-			kurstermin.save()
 	pass
