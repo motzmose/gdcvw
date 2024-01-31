5c45579545c00bc7fd8124ccc82fb8a4e6d1e6b6
Moritz Kaimann <kaimann@didaktik-aktuell.de>
2024-01-31 14:25:24 +0100

    potential version conflict

--- gdc/gdc/doctype/kurs_erstellen/kurs_erstellen.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, didaktik-aktuell e.V. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Kurs erstellen", {
-    refresh(frm) {
-        frm.disable_save();
-    },
-});
--- gdc/gdc/doctype/kurs_erstellen/test_kurs_erstellen.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, didaktik-aktuell e.V. and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestKurserstellen(FrappeTestCase):
-	pass
