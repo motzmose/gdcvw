6724799f181898a0021b09798da96e8408e31abe
Moritz Kaimann <kaimann@didaktik-aktuell.de>
2024-01-31 14:18:00 +0100

    fix patch

--- gdc/gdc/doctype/kurs/patches/drop_doctypes_gruppe_and_implement_kurs.py
+++ gdc/gdc/doctype/kurs/patches/drop_doctypes_gruppe_and_implement_kurs.py
@@ -4,6 +4,5 @@ import frappe
 def execute():
 	"""drop doctypes gruppe and implement kurs"""
 	frappe.reload_doc("gdc", "doctype", "kurs")
-	frappe.reload_doc("gdc", "doctype", "kurs erstellen")
+	frappe.reload_doc("gdc", "doctype", "kurs_erstellen")
 	frappe.reload_doc("gdc", "doctype", "kursteilnehmerin")
-	frappe.reload_doc("gdc", "doctype", "gruppe")
